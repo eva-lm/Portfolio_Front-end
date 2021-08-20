70a0583ded1515a91bdc8cc6fada1418a24703cd
John Doe <eva.lm1810@gmail.com>
2021-08-20 15:00:00 +0200

    add more ux info

--- src/components/Ux.js
+++ src/components/Ux.js
@@ -1,62 +1,97 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "../stylesheets/layout/Ux.scss";
-import { animateScroll as scroll} from "react-scroll";
 import { scroller } from "react-scroll";
 import { Element } from "react-scroll";
 import { Events,scrollSpy } from "react-scroll";
 import { projectsUx } from "./projectsUx";
 
 export const Ux = () => {
-  // window.onscroll = function (e) {  
-  //   var element = document.getElementById("nav");
-  //   element.classList.add("change");
-  // } 
 const [currentPage, setCurrentPage] = useState(Math.abs(0))
-const [hidden, setHidden] = useState(false)
+const [showViewComplete, setShowViewComplete] = useState(false)
 
-function onClickDown() {
-    scroller.scrollTo("project");
- }
 
  function showNextProject(event) {
   if (currentPage <= projectsUx.length-2) {
     setCurrentPage(Math.abs(Number(event.target.id) +1))
+    setShowViewComplete(false)
   }
  }
  function showPreviousProject(event) {
   if (currentPage >= 1) {
     setCurrentPage(Math.abs(Number(event.target.id) -1))
+    setShowViewComplete(false)
     }
  }
-const showMoreInfo = () => {
-setHidden(!hidden)
+function showMoreInfo() {
+  setShowViewComplete(!showViewComplete)
 }
+useEffect(() => {
+  window.scroll({
+    top: document.body.offsetHeight,
+    left: 0, 
+    behavior: 'smooth',
+  }) 
+
+}, [showViewComplete])
+
+function goScroll() {
+  scroller.scrollTo("project");
+}
+
 console.log(" projectsUx.length->",  projectsUx.length ,"y currentpage", currentPage)
 // console.log("parseInt(projectsUx[currentPage].id)", parseInt(projectsUx[currentPage].id))
   return (
-    <section className="ux">  
+    <section className={showViewComplete ? "ux ux-complete" : "ux"}>  
     {parseInt(projectsUx[currentPage].id) !== undefined && projectsUx.length ?
        ( <article className="ux__project pink" id={parseInt(projectsUx[currentPage].id)} style={{ display: "flex", flexDirection: "column"}}>
           <h2>{projectsUx[currentPage].title}</h2>
+          <div className="ux__project-content">
+        <input type="button" className="ux__project-arrow-slider" id={parseInt(projectsUx[currentPage].id)} onClick={showPreviousProject} value="<" />
+    
               <img className="ux__img" src={projectsUx[currentPage].image} alt={projectsUx[currentPage].title} />
-        <input type="button" id={parseInt(projectsUx[currentPage].id)} style={{ width: "100px"}} onClick={showPreviousProject} />Left
-        <input type="button" id={parseInt(projectsUx[currentPage].id)} style={{ width: "100px"}} onClick={showNextProject} />Right
-        <button onClick={showMoreInfo}>
-          show more...
+        <input type="button" className="ux__project-arrow-slider" id={parseInt(projectsUx[currentPage].id)}onClick={showNextProject} value=">"/>
+        </div>
+        <button style={{ fontSize: "18px"}}  className="about__button" onClick={showMoreInfo}>
+          Show more...
         </button>
 
-        {hidden ?
+        {showViewComplete ?
         <>
        {projectsUx[currentPage].info.map((inf, indx) => {
          return (
-           <div key={indx}>
-           <img height="60" src={inf.style} />
-           <p>{inf.description}</p>
-           <video width="400" controls autoPlay>
-           <source src={inf.video} type="video/mp4" />
-         Your browser does not support the video tag.
-         </video>
-           </div>
+          <div className="ux-complete__content" key={indx}>
+              <div className="ux-complete__content-info">
+                <div className="ux-complete__content-info-main">
+                    <h3 className="ux-complete__content-info-main-title">Demo:</h3>
+                    <p className="ux-complete__content-info-main-description">{inf.description}</p>
+                </div>
+                <video width="50%" controls autoPlay>
+                  <source src={inf.video} type="video/mp4" />
+                  Your browser does not support the video tag.
+                </video>
+              </div> 
+
+              <div className="ux-complete__content-info">
+                <div className="wrap-img">
+                    <img className="ux-complete__img-wireframe" src={inf.wireframe} />
+                </div>
+                <div className="ux-complete__content-info-wireframe">
+                  <h4 className="ux-complete__content-info-wireframe-title">Wireframe:</h4>
+                  <p className="ux-complete__content-info-wireframe-description">{inf.descriptionWireframe}</p>
+                </div>
+              </div>
+
+              <div className="ux-complete__content-info">
+                <div className="ux-complete__content-info-style">
+                  <h4 className="ux-complete__content-info-style-title">Hoja de estilo:</h4>
+                  <p className="ux-complete__content-info-style-description">{inf.descriptionStyle}</p>
+                </div>
+                <div className="wrap-img">
+                  <img className="ux-complete__img-style" src={inf.style} />
+                </div>
+              </div>
+
+          </div>
          )
        })}
        </>
--- src/components/projectsUx.js
+++ src/components/projectsUx.js
@@ -1,8 +1,14 @@
 import React from "react";
 import ux1 from "../images/ux/ecoCream/ux1.png";
 import ux2 from "../images/ux/ux2.png";
+import ux3 from "../images/ux/fashionTrends/ux3.png";
 import styleUx1 from "../images/ux/ecoCream/style/style-ux1.png";
 import videoUx1 from "../images/ux/ecoCream/video/ecocream-video.mov";
+import videoUx3 from "../images/ux/fashionTrends/video/fashiontrends-video.mov";
+
+import wireframeUx1 from "../images/ux/ecoCream/wire/Wireframe_EcoCream.png";
+
+
 
 export const projectsUx = [
         {
@@ -11,11 +17,12 @@ export const projectsUx = [
             "image": ux1,
                "info": [
                     {
-                        "wireframe1": "",
-                        "wireframe2": "",
-                        "style": styleUx1,
-                        "description": "EcoCream, es un proyecto personal que consiste en el diseño de una web sobre cósmetica vegana. Los colores usados tienen consonancia con lo natural y la cosmética, con un estilo que recuerde a la tierra y el bienstar junto con una tipografía sencilla para representar al producto. Proyecto realizado en Marvel App.",
+                        "description": "EcoCream, es un proyecto personal que consiste en el diseño de una web sobre cósmetica vegana. Se basa en la venta on-line de productos que son 100% naturales y respetuosos con el medio ambiente.",
                         "video": videoUx1,
+                        "wireframe": wireframeUx1,
+                        "descriptionWireframe": "Boceto de wireframes de baja fidelidad para después elaborar el diseño en MarvelApp.",
+                        "style": styleUx1,
+                        "descriptionStyle": "Los colores usados tienen consonancia con lo natural y la cosmética, con un estilo que recuerde a la tierra y el bienstar junto con una tipografía sencilla para representar al producto."
                     } 
                 ] 
         },
@@ -29,6 +36,17 @@ export const projectsUx = [
                         "mock2": "",
                         //"style": "../images/style/style-ux2.jpeg",
                         "description": "Diseño de una landing page que consiste en la venta de joyas artesanas. Estilo fresco y desenfadado focalizado en público que le guste llevar complementos únicos y hechos a mano. También se trata de un proyecto personal, realizado con Adobe XD. "
+                    } 
+                ] 
+        },
+        {
+            "id": "2",
+            "title": "Fashion Trends",
+            "image": ux3,
+               "info": [
+                    {
+                        "description": "Diseño de una app mobile ficticia que consiste en una aplicación de moda en la que se comparten tendencias y tienes la opción de comprar las prendas que aparecen en las fotos de la gente a la que sigues, generando enlaces externos hacía la tienda.",
+                        "video": videoUx3
                     } 
                 ] 
         }
